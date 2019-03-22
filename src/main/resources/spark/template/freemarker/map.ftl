e1018db345e0c4a4a13b92b8dd00df1f536185be
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-03-22 00:19:18 -0400

    Add Mapbox to home screen

--- src/main/resources/spark/template/freemarker/map.ftl
+++ src/main/resources/spark/template/freemarker/map.ftl
@@ -1,16 +1,21 @@
 <#assign content>
 
+<div style="width: 300px; text-align: center; position: absolute; left: 0; top: 40%; right: 0; margin-left: auto; margin-right: auto;" id = "loading">
+    <img src ="/images/loading.gif" style="width: 60px; height: auto;"/>
+</div>
+
 <div style="visibility: hidden;" class="search-menu-wrapper">
     <div class="search-menu">
         <h2 class="section-heading">Search for a Carpool</h2>
         <div class="search-inputs">
             <div><i class="fas fa-dot-circle icon-label"></i>
-                <input class="address-input" id="start-input" oninput="handleInput()" type="text" placeholder="Starting point...">
+                <input class="address-input" id="start-input" onblur="handleInput()" type="text" placeholder="Starting point...">
             </div>
             <i class="fab fa-nintendo-switch icon-switch" onclick="switchAddresses()"></i>
             <div><i class="fas fa-map-marker-alt icon-label"></i>
-                <input class="address-input" id="end-input" oninput="handleInput()" type="text" placeholder="Ending destination...">
+                <input class="address-input" id="end-input" onblur="handleInput()" type="text" placeholder="Ending destination...">
             </div>
+            <button onclick="addMarkerTest()">Add marker</button>
         </div>
     </div>
 </div>
@@ -24,7 +29,6 @@
 
 <script src="/js/jquery-3.1.1.js"></script>
 <script src="/js/map.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDUkO8cyOm9dCtWhDkAwdZyfjwsBegrXd4&callback=initMap" async defer></script>
 
 </#assign>
 <#include "main.ftl">
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,20 +1,69 @@
 let map;
+let geocoder;
 
 let curLat = 42.358188;
 let curLong = -71.058502;
+
+$(document).ready(function () {
+    initMap();
+    console.log("DOM ready.");
+});
+
 
 function centerMap() {
     moveToLocation(curLat, curLong);
 }
 
-function moveToLocation(lat, lng) {
-    let center = new google.maps.LatLng(lat, lng);
-    map.panTo(center);
+function addMarkerTest() {
+    addMarker(42.358188, -71.058502, "test");
+    moveToLocation(42.358188, -71.058502);
 }
 
-function handleInput() {
-    console.log("handled!");
+function addMarker(lat, long, name) {
+    let mapLayer = map.getLayer(name);
+    if (typeof mapLayer === 'undefined') {
+        /* Image: An image is loaded and added to the map. */
+        map.loadImage("https://i.imgur.com/MK4NUzI.png", function (error, image) {
+            if (error) throw error;
+            map.addImage("custom-marker", image);
+            /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+            map.addLayer({
+                id: name,
+                type: "symbol",
+                /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+                source: {
+                    type: "geojson",
+                    data: {
+                        type: 'FeatureCollection',
+                        features: [{
+                            type: 'Feature',
+                            properties: {},
+                            geometry: {
+                                type: "Point",
+                                coordinates: [long, lat]
+                            }
+                        }]
+                    }
+                },
+                layout: {
+                    "icon-image": "custom-marker",
+                }
+            });
+        });
+    } else {
+        console.log("Marker already exists.");
+    }
 }
+
+function moveToLocation(lat, lng) {
+    map.flyTo({
+        center: [
+            lng, lat
+        ]
+    });
+}
+
+function handleInput() {}
 
 function switchAddresses() {
     let start = $("#start-input").val();
@@ -36,15 +85,20 @@ function initMap() {
 }
 
 function renderMap(lat, long) {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: lat,
-            lng: long,
-        },
-        zoom: 15,
-        disableDefaultUI: true
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ2Mzk1IiwiYSI6ImNqdGpodWcwdDB6dXEzeXBrOHJyeGVpNm8ifQ.bAwH-KG_5A5kwIxCf6xCSQ';
+    map = new mapboxgl.Map({
+        container: 'map',
+        keyboard: false,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [long, lat], // starting position [lng, lat]
+        zoom: 12
     });
+    $(".loading").css({
+        visibility: "hidden"
+    });
+
     $(".search-menu-wrapper").css({
         visibility: "visible"
     });
+    console.log("Map loaded.");
 }
