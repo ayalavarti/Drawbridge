706725d432c5456627b894d8f9483b0d02bb42cb
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-03-22 16:04:44 -0400

    Add comments to JS files

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,13 +1,26 @@
+// Store user profile across JS files
 let userProfile;
 
+/**
+ * Handles sign in errors.
+ *
+ * @param {*} error
+ */
 function onFailure(error) {
 	console.log(error);
 }
 
+/**
+ * Handles successful sign in requests.
+ *
+ * @param {*} googleUser
+ */
 function onSignIn(googleUser) {
+	// Store userprofile in global variable
 	userProfile = googleUser;
 	let profile = googleUser.getBasicProfile();
 
+	// Add profile picture
 	$("#profile-picture-wrapper").prepend(
 		$("<img>", {
 			id: "profile-picture",
@@ -15,7 +28,11 @@ function onSignIn(googleUser) {
 			onerror: "this.onerror=null;this.src='images/temp.png';"
 		})
 	);
+
+	// Set user name
 	$("#user-name").text(profile.getGivenName());
+
+	// Hide the sign in button and show the profile info button
 	setTimeout(function () {
 		$("#profile-info").css({
 			visibility: "visible"
@@ -24,11 +41,17 @@ function onSignIn(googleUser) {
 	}, 500);
 }
 
+/**
+ * Sign out the user.
+ */
 function signOut() {
 	let auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
 		console.log("User signed out.");
+		// Reset userProfile variable
 		userProfile = undefined;
+
+		// Hide profile info dropdown and show login button
 		$("#profile-info").css({
 			visibility: "hidden"
 		});
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,6 +1,12 @@
+/**
+ * Set up global variables for use in all map actions.
+ */
 let map;
 let mapboxClient;
 
+/**
+ * Hardcoded starting location - will replace later.
+ */
 let curLat = 42.358188;
 let curLong = -71.058502;
 
@@ -9,12 +15,19 @@ let addressNames = [];
 let coordinates = [];
 let markers = [];
 
+/**
+ * When the DOM loads, initialize Mapbox and the Map object.
+ */
 $(document).ready(function () {
     initMapbox();
     initMap();
     console.log("DOM ready.");
 });
 
+/**
+ * Initializes the Mapbox using the accessToken and sets up the
+ * mapboxClient for use in Geolocating.
+ */
 function initMapbox() {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ2Mzk1IiwiYSI6ImNqdGpodWcwdDB6dXEzeXBrOHJyeGVpNm8ifQ.bAwH-KG_5A5kwIxCf6xCSQ';
     mapboxClient = mapboxSdk({
@@ -22,34 +35,52 @@ function initMapbox() {
     });
 }
 
+/**
+ * Initializes the Map.
+ */
 function initMap() {
-    try {
-        map = new mapboxgl.Map({
-            container: 'map',
-            keyboard: false,
-            maxZoom: 18,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [curLong, curLat],
-            zoom: 12
-        });
-        map.addControl(new mapboxgl.NavigationControl());
+    // Create map object with custom settings and add NavigationControl
+    map = new mapboxgl.Map({
+        container: 'map',
+        keyboard: false,
+        maxZoom: 18,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [curLong, curLat],
+        zoom: 12
+    });
+    map.addControl(new mapboxgl.NavigationControl());
 
-        $("#loading").css({
-            visibility: "hidden"
-        });
-        $("[id=pre-load]").css({
-            visibility: "visible"
-        });
-        console.log("Map loaded.");
-
-    } catch (err) {
-        console.log("Map load error.");
-    }
+    /**
+     * Hide loading gif and show all initially hidden objects
+     */
+    $("#loading").css({
+        visibility: "hidden"
+    });
+    $("[id=pre-load]").css({
+        visibility: "visible"
+    });
+    console.log("Map loaded.");
 }
 
+/**
+ * Handle changes to the address input boxes whenever the input box loses focus.
+ *
+ * @param {*} id
+ *            The name of the input box (either 'start-input' or 'end-input')
+ * @param {*} index
+ *            The index to use for identification (either 0 or 1)
+ */
 function handleInput(id, index) {
+    // Get the address value from the correct input box
     let address = $(`#${id}`).val()
+    if (address === "") {
+        removeMarker(index);
+    }
+    /**
+     * Check if address is empty or if address hasn't changed
+     */
     if (address != "" && addressNames[index] != address) {
+        // Send network request for geocoding based on address box value
         mapboxClient.geocoding.forwardGeocode({
                 query: address,
                 proximity: [curLong, curLat],
@@ -58,23 +89,47 @@ function handleInput(id, index) {
             })
             .send()
             .then(function (response) {
+                // If valid response
                 if (response && response.body && response.body.features &&
                     response.body.features.length) {
-
+                    /**
+                     * Get the first element of the suggestions, set the input box to that
+                     * value, then update the addressNames and coordinates arrays with the
+                     * feature data.
+                     * */
                     let feature = response.body.features[0];
                     $(`#${id}`).val(feature.place_name);
                     addressNames[index] = feature.place_name;
                     coordinates[index] = feature.center;
+                    // Add new marker on the map with the returned feature data
                     addStreetPoint(feature.center[1], feature.center[0], id, index);
                 }
             });
     }
 }
 
+/**
+ * Add a new street point on the map after a new address is inputted.
+ *
+ * @param {*} lat
+ *            The marker center latitude.
+ * @param {*} long
+ *            The marker center longitude.
+ * @param {*} id
+ *            The name of the input box (either 'start-input' or 'end-input')
+ * @param {*} index
+ *            The index to use for identification (either 0 or 1)
+ */
 function addStreetPoint(lat, long, id, index) {
-    addMarker2(lat, long, id, index);
+    /**
+     * Add a new marker on the map then mark the associated input type as found
+     */
+    addMarker(lat, long, id, index);
     found[index] = true;
 
+    /**
+     * If both
+     */
     if (found[0] && found[1]) {
         map.fitBounds(coordinates, {
             padding: {
@@ -91,14 +146,25 @@ function addStreetPoint(lat, long, id, index) {
     }
 }
 
-function addMarker2(lat, long, id, index) {
+/**
+ * Add marker on the map with the given parameters.
+ *
+ * @param {*} lat
+ *            The marker center latitude.
+ * @param {*} long
+ *            The marker center longitude.
+ * @param {*} id
+ *            The name of the input box (either 'start-input' or 'end-input')
+ * @param {*} index
+ *            The index to use for identification (either 0 or 1)
+ */
+function addMarker(lat, long, id, index) {
     let el = document.createElement('div');
     el.className = `marker ${id}`;
     if (id == "start-input") {
         el.className = el.className + " pulse";
     }
     if (markers[index]) {
-        console.log("Existing marker exists.");
         markers[index].remove();
     }
     markers[index] = new mapboxgl.Marker(el)
@@ -106,6 +172,24 @@ function addMarker2(lat, long, id, index) {
     markers[index].addTo(map);
 }
 
+/**
+ * Removes a marker from the map given index to remove.
+ *
+ * @param {*} index
+ *            Index to remove
+ */
+function removeMarker(index) {
+    if (markers[index]) {
+        markers[index].remove();
+        delete markers[index];
+        delete coordinates[index];
+        found[index] = false;
+    }
+}
+
+/**
+ * Centers the map view.
+ */
 function centerMap() {
     map.flyTo({
         center: [
@@ -117,15 +201,23 @@ function centerMap() {
     });
 }
 
+/**
+ * Realigns the map view to north and 0 tilt.
+ */
 function alignMap() {
     map.flyTo({
         pitch: 0,
-        bearing: 0,
-        zoom: 12
+        bearing: 0
     });
 }
 
-
+/**
+ * Moves the map view to a new center location with the
+ * provided arguments.
+ *
+ * @param {*} lat
+ * @param {*} lng
+ */
 function moveToLocation(lat, lng) {
     map.flyTo({
         center: [
