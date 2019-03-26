afbb537a39da66e66b5ba649c19c08e1fc07737a
Jeffrey Zhu <jeffrey_zhu@brown.edu>
2019-03-26 13:56:17 -0400

    uncomment DatabaseQueryTest

--- src/test/java/edu/brown/cs/drawbridge/database/DatabaseQueryTest.java
+++ src/test/java/edu/brown/cs/drawbridge/database/DatabaseQueryTest.java
@@ -2,24 +2,13 @@ package edu.brown.cs.drawbridge.database;
 
 public class DatabaseQueryTest {
 
-<<<<<<< HEAD
-  // @Test
-  // public void testDatabaseConnection() {
-  // try {
-  // DatabaseQuery test = new DatabaseQuery("//localhost/carpools");
-  // } catch (ClassNotFoundException | SQLException e) {
-  // assert false;
-  // }
-  // }
-=======
   //@Test
   public void testDatabaseConnection() {
-    try {
-      DatabaseQuery test = new DatabaseQuery("//localhost/carpools", "", "");
-    } catch (ClassNotFoundException | SQLException e) {
-      assert false;
-    }
+  try {
+  DatabaseQuery test = new DatabaseQuery("//localhost/carpools", "", "");
+  } catch (ClassNotFoundException | SQLException e) {
+  assert false;
   }
->>>>>>> 67f20ca7fec920066e65919d24400dddd6f4ada0
+  }
 
 }
