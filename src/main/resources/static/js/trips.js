208ccd4baf92a573885edce3c334b67a54a06da3
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-03-27 13:31:18 -0400

    Add basic card dropdown

--- src/main/resources/static/js/trips.js
+++ src/main/resources/static/js/trips.js
@@ -1,34 +1,58 @@
-$(document).ready(function () {
-    signInTooltip[0].setContent("Sign in with your Google Account to view your trips.");
-    if (navigator.cookieEnabled) {
-        if (getCookie("loggedIn") != "true") {
-            signInTooltip[0].show();
-        }
-    } else {
-        signInTooltip[0].show();
-    }
-    showHomeInfo();
+$(document).ready(function() {
+	signInTooltip[0].setContent("Sign in with your Google Account to view your trips.");
+	if (navigator.cookieEnabled) {
+		if (getCookie("loggedIn") != "true") {
+			signInTooltip[0].show();
+		}
+	} else {
+		signInTooltip[0].show();
+	}
+	showHomeInfo();
 });
 
-
 function onUserSignedIn() {
-    $("#my-trips-wrapper").css({
-        visibility: "visible"
-    })
-    $("#trips-picture").attr({
-        src: `${userProfile.getImageUrl()}`,
-        onerror: "this.onerror=null;this.src='/images/temp.png';"
-    });
-
-    signInTooltip[0].hide();
+	$("#my-trips-wrapper").css({
+		visibility: "visible"
+	});
+	$("#trips-picture").attr({
+		src: `${userProfile.getImageUrl()}`,
+		onerror: "this.onerror=null;this.src='/images/temp.png';"
+	});
+	signInTooltip[0].hide();
 }
 
 function onUserSignedOut() {
-    $("#my-trips-wrapper").css({
-        visibility: "hidden"
-    })
-    $("#trips-picture").attr({
-        src: "/images/temp.png",
-    });
-    signInTooltip[0].show();
+	$("#my-trips-wrapper").css({
+		visibility: "hidden"
+	});
+	$("#trips-picture").attr({
+		src: "/images/temp.png"
+	});
+	signInTooltip[0].show();
+}
+
+function showGroup(ids) {
+	let start = 0;
+	ids.reverse().forEach(element => {
+		$(`#${element}`).animate(
+			{
+				bottom: `${start}px`
+			},
+			"slow"
+		);
+		start = start + 150;
+	});
+}
+
+function hideGroup(ids) {
+	let bottom = 150 * ids.length;
+
+	ids.forEach(element => {
+		$(`#${element}`).animate(
+			{
+				bottom: `${bottom}px`
+			},
+			"slow"
+		);
+	});
 }
